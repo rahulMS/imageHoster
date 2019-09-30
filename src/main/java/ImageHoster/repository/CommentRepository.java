82d48495a5c7cf7a10237dc4a25a63ebf4e5c405
raanand <raanand@akamai.com>
2019-09-30 22:01:06 +0530

    removing pwd check login and correcting few issues

--- src/main/java/ImageHoster/repository/CommentRepository.java
+++ src/main/java/ImageHoster/repository/CommentRepository.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ImageHoster.repository;
 
 import ImageHoster.model.Comment;
@@ -20,7 +15,7 @@ public class CommentRepository {
     @PersistenceUnit(unitName = "imageHoster")
     private EntityManagerFactory emf;
 
-    public Comment insertComment(Comment newComment) {
+    public Comment addComment(Comment newComment) {
 
         EntityManager em = emf.createEntityManager();
         EntityTransaction transaction = em.getTransaction();
